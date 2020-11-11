d02b1242644267d3e43c385b7cd60ed9a1c47872
SylvainG <sguerrin549@gmail.com>
2020-11-11 09:43:37 +0100

    add manage of biker registration forms

--- src/Controller/Biker/BikerController.php
+++ src/Controller/Biker/BikerController.php
@@ -2,9 +2,20 @@
 
 namespace App\Controller\Biker;
 
+use App\Entity\Biker;
+use App\Entity\User;
+use App\Form\RegistrationBikerType;
+use App\Form\RegistrationCityType;
+use App\Form\RegistrationType;
+use App\Repository\BikerRepository;
 use App\Services\ManageBikerMultiStepsFormService;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\Form\FormFactoryInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Twig\Environment;
 use Twig\Error\LoaderError;
 use Twig\Error\RuntimeError;
@@ -14,16 +25,30 @@ class BikerController
 {
     private $twig;
     private $bikerMultiStepsFormService;
+    private $form;
+    private $request;
+    private $doctrine;
 
     /**
      * BikerController constructor.
      * @param Environment $twig
      * @param ManageBikerMultiStepsFormService $bikerMultiStepsFormService
+     * @param FormFactoryInterface $form
+     * @param RequestStack $request
+     * @param ManagerRegistry $registry
      */
-    public function __construct(Environment $twig, ManageBikerMultiStepsFormService $bikerMultiStepsFormService)
+    public function __construct(
+        Environment $twig,
+        ManageBikerMultiStepsFormService $bikerMultiStepsFormService,
+        FormFactoryInterface $form,
+        RequestStack $request,
+        ManagerRegistry $registry)
     {
         $this->twig = $twig;
         $this->bikerMultiStepsFormService = $bikerMultiStepsFormService;
+        $this->form = $form;
+        $this->request = $request;
+        $this->doctrine = $registry;
     }
 
     /**
@@ -39,12 +64,39 @@ class BikerController
 
     /**
      * @Route("/biker-inscription/etape-une", name="biker_registration_step_one")
+     * @param UserPasswordEncoderInterface $userPasswordEncoder
+     * @return Response
      * @throws LoaderError
      * @throws RuntimeError
      * @throws SyntaxError
      */
-    public function stepOne(): Response
+    public function stepOne(UserPasswordEncoderInterface $userPasswordEncoder): Response
     {
+        //add logic for biker multi form service
+        $this->bikerMultiStepsFormService->verifyStepInSession($step = 'one');
+
+        $user = new User();
+
+        $form = $this->form->create(RegistrationType::class, $user);
+        $form->handleRequest($this->request->getCurrentRequest());
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $hashPass = $userPasswordEncoder->encodePassword($user, $user->getPassword());
+            $user->setPassword($hashPass);
+            $user->setCreatedAt(new \DateTimeImmutable('now'));
+            $user->setRole(['ROLE_USER']);
+
+            $em = $this->doctrine->getManager();
+            $em->persist($user);
+            $em->flush();
+//            $this->session->getFlashBag()->add(
+//                'success',
+//                'Votre compte a bien été créé ! Vous pouvez maintenant vous connecter !'
+//            );
+
+            return new RedirectResponse('biker_registration_step_two');
+        }
+
         return new Response($this->twig->render('biker/account/registration/registration-step-one.html.twig'));
     }
 
@@ -56,17 +108,64 @@ class BikerController
      */
     public function stepTwo(): Response
     {
+        //add logic for biker multi form service
+        $this->bikerMultiStepsFormService->verifyStepInSession($step = 'two');
+
+        $biker = new Biker();
+
+        $form = $this->form->create(RegistrationBikerType::class, $biker);
+        $form->handleRequest($this->request->getCurrentRequest());
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            //don't forget relation with user, catch id with session
+
+            $em = $this->doctrine->getManager();
+            $em->persist($biker);
+            $em->flush();
+//            $this->session->getFlashBag()->add(
+//                'success',
+//                'Votre compte a bien été créé ! Vous pouvez maintenant vous connecter !'
+//            );
+
+            return new RedirectResponse('biker_registration_step_three');
+        }
+
         return new Response($this->twig->render('biker/account/registration/registration-step-two.html.twig'));
     }
 
     /**
      * @Route("/biker-inscription/etape-trois", name="biker_registration_step_three")
+     * @param BikerRepository $bikerRepository
+     * @return Response
      * @throws LoaderError
      * @throws RuntimeError
      * @throws SyntaxError
      */
-    public function stepThree(): Response
+    public function stepThree(BikerRepository $bikerRepository): Response
     {
+        //add logic for biker multi form service
+//        $bikerId = $this->bikerMultiStepsFormService
+//        $biker = $bikerRepository->find($bikerId);
+
+        $form = $this->form->create(RegistrationCityType::class, $biker);
+        $form->handleRequest($this->request->getCurrentRequest());
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            //don't forget relation with biker, catch id with session
+            //and add city to biker entity
+
+            $em = $this->doctrine->getManager();
+            $em->persist($biker);
+//            $this->session->getFlashBag()->add(
+//                'success',
+//                'Votre compte a bien été créé ! Vous pouvez maintenant vous connecter !'
+//            );
+
+            return new RedirectResponse('login');
+        }
+
         return new Response($this->twig->render('biker/account/registration/registration-step-three.html.twig'));
     }
 }
--- src/Services/ManageBikerMultiStepsFormService.php
+++ src/Services/ManageBikerMultiStepsFormService.php
@@ -3,8 +3,49 @@
 
 namespace App\Services;
 
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
+/**
+ * to manage session between forms, initialize at each pages
+ * Class ManageBikerMultiStepsFormService
+ * @package App\Services
+ */
 class ManageBikerMultiStepsFormService
 {
+    protected $session;
 
+    /**
+     * ManageBikerMultiStepsFormService constructor.
+     * @param SessionInterface $session
+     */
+    public function __construct(SessionInterface $session)
+    {
+        $this->session = $session;
+    }
+
+    /**
+     * @param $step
+     * @return RedirectResponse|void
+     */
+    public function verifyStepInSession($step)
+    {
+        if($step === 'one'){
+            if ($this->session->has('stepOne')) {
+                return new RedirectResponse('biker_registration_step_two');
+            }
+        }
+
+        if($step === 'two'){
+            if($this->session->has('stepTwo')) {
+                return new RedirectResponse('biker_registration_step_three');
+            }
+        }
+
+        if($step === 'three'){
+            if(!$this->session->has('stepOne') && !$this->session->has('stepTwo')) {
+                return new RedirectResponse('biker_registration_step_one');
+            }
+        }
+    }
 }
