2be5f55440a997134a6f35b6affb67c6950997cd
SylvainG <sguerrin549@gmail.com>
2021-01-14 13:14:09 +0100

    finished owner registration page

--- public/site/js/owner/registration-owner.js
+++ public/site/js/owner/registration-owner.js
@@ -1,10 +1,43 @@
 //owl-carousel
-$(".owl-carousel").owlCarousel({
+$(".owl-present").owlCarousel({
     center:true,
     // items:4,
     loop:false,
     rewind:true,
     margin: 20,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:false
+        },
+        600:{
+            items:1,
+            nav:false
+        },
+        900:{
+            items:1,
+            nav:false
+        },
+        1200:{
+            items:1,
+            nav:false
+        }
+    },
+    autoHeight:false,
+    autoplay:true,
+    autoplayTimeout:5000,
+    autoplayHoverPause:true,
+    dots:false,
+    nav:false,
+});
+
+$(".owl-partnership").owlCarousel({
+    center:true,
+    // items:4,
+    loop:false,
+    rewind:true,
+    // margin: 20,
     responsiveClass:true,
     responsive:{
         0:{
